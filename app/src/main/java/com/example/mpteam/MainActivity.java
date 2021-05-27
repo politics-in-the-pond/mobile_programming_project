1dea50cf19e336fb99d2a757074392de53988668
khjj07 <khjj07@gmail.com>
2021-05-28 05:29:16 +0900

    menu slide 완성

--- app/src/main/java/com/example/mpteam/MainActivity.java
+++ app/src/main/java/com/example/mpteam/MainActivity.java
@@ -2,17 +2,29 @@ package com.example.mpteam;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends FragmentActivity {
+    private static final int NUM_PAGES = 3;
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pagerAdapter;
     BottomNavigationView bottomNavigationView;
     BoxFragment boxFragment;
     DiaryFragment diaryFragment;
     MyPageFragment myPageFragment;
+    ArrayList<Fragment> fragment_list=new ArrayList<>();
+    int [] menu_item_list={R.id.mypage,R.id.diary,R.id.box};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,25 +35,55 @@ public class MainActivity extends AppCompatActivity {
         boxFragment = new BoxFragment();
         diaryFragment = new DiaryFragment();
         myPageFragment = new MyPageFragment();
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,myPageFragment).commitAllowingStateLoss();
+        fragment_list.add(myPageFragment);
+        fragment_list.add(diaryFragment);
+        fragment_list.add(boxFragment);
+        viewPager = findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setCurrentItem(1);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()){
                     case R.id.mypage:{
-                        getSupportFragmentManager().beginTransaction() .replace(R.id.main_layout,myPageFragment).commitAllowingStateLoss();
+                        viewPager.setCurrentItem(0);
                         return true;
                     } case R.id.diary:{
-                        getSupportFragmentManager().beginTransaction() .replace(R.id.main_layout,diaryFragment).commitAllowingStateLoss();
+                        viewPager.setCurrentItem(1);
                         return true;
                     } case R.id.box:{
-                        getSupportFragmentManager().beginTransaction() .replace(R.id.main_layout,boxFragment).commitAllowingStateLoss();
+                        viewPager.setCurrentItem(2);
                         return true;
 
                     }default: return false;
                 }
             }
         });
+        viewPager.registerOnPageChangeCallback( new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int pos) {
+                super.onPageSelected(pos);
+                bottomNavigationView.getMenu().getItem(pos).setChecked(true);
+            }
+        });
+    }
+    public void onBackPressed() {
+        finish();
+    }
+    private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+        public ScreenSlidePagerAdapter(FragmentActivity fa) {
+            super(fa);
+        }
 
+        @Override
+        public Fragment createFragment(int position) {
+            bottomNavigationView.getMenu().getItem(position).setChecked(true);
+            return fragment_list.get(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
     }
 }
