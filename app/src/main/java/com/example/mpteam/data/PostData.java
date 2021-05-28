867a138de552aa1f4ddb37f2176a402fb80f8af6
Yongho Bae <yonghobae12@naver.com>
2021-05-28 15:57:30 +0900

    Lombok 기능 추가및 DataBinding 사용 + 일기작성기능 개발중

--- app/src/main/java/com/example/mpteam/data/PostData.java
+++ app/src/main/java/com/example/mpteam/data/PostData.java
@@ -2,107 +2,18 @@ package com.example.mpteam.data;
 
 import java.util.ArrayList;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostData {
     String postId, userId, title, content, dateTime;
     double latitude, longitude;
     boolean ispublic;
     ArrayList<String> imageURL;
     int emotion; //0기쁨 1보통 2우울 3미묘 이런 형태로
-
-    public PostData(String postId, String userId, String title, String content, String dateTime, double latitude, double longitude, boolean ispublic, ArrayList<String> imageURL, int emotion) {
-        this.postId = postId;
-        this.userId = userId;
-        this.title = title;
-        this.content = content;
-        this.dateTime = dateTime;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.ispublic = ispublic;
-        this.imageURL = imageURL;
-        this.emotion = emotion;
-    }
-
-    public String getPostId() {
-        return postId;
-    }
-
-    public boolean isIspublic() {
-        return ispublic;
-    }
-
-    public void setIspublic(boolean ispublic) {
-        this.ispublic = ispublic;
-    }
-
-    public void setPublicKey(boolean ispublic) {
-        this.ispublic = ispublic;
-    }
-
-    public void setPostId(String postId) {
-        this.postId = postId;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public String getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(String dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public ArrayList<String> getImageURL() {
-        return imageURL;
-    }
-
-    public void setImageURL(ArrayList<String> imageURL) {
-        this.imageURL = imageURL;
-    }
-
-    public int getEmotion() {
-        return emotion;
-    }
-
-    public void setEmotion(int emotion) {
-        this.emotion = emotion;
-    }
 }
--- app/src/main/java/com/example/mpteam/data/UserData.java
+++ app/src/main/java/com/example/mpteam/data/UserData.java
@@ -1,48 +1,18 @@
 package com.example.mpteam.data;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class UserData {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserData implements Serializable {
     String userId, nickname;
     ArrayList<String> userPosts;
     ArrayList<Integer> cardIndex;
-
-    public UserData(String userId, String nickname, ArrayList<String> userPosts, ArrayList<Integer> cardIndex){
-        this.userId = userId;
-        this.nickname = nickname;
-        this.userPosts = userPosts;
-        this.cardIndex = cardIndex;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public ArrayList<String> getUserPosts() {
-        return userPosts;
-    }
-
-    public void setUserPosts(ArrayList<String> userPosts) {
-        this.userPosts = userPosts;
-    }
-
-    public ArrayList<Integer> getCardIndex() {
-        return cardIndex;
-    }
-
-    public void setCardIndex(ArrayList<Integer> cardIndex) {
-        this.cardIndex = cardIndex;
-    }
 }
