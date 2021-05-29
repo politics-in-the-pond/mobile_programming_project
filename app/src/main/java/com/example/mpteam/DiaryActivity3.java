c76a53071f845a17ba8ab5494d31ead8b3c40d78
Yongho Bae <yonghobae12@naver.com>
2021-05-29 18:34:21 +0900

    지도기능  연동 추가 초기에는 자신의 위치를 시작점으로 함

--- app/src/main/java/com/example/mpteam/DiaryActivity3.java
+++ app/src/main/java/com/example/mpteam/DiaryActivity3.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Binder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,14 +14,22 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingComponent;
 import androidx.databinding.DataBindingUtil;
+import androidx.databinding.Observable;
 
 import com.example.mpteam.data.PostData;
 import com.example.mpteam.databinding.ActivityDiary3Binding;
 import com.google.android.gms.common.util.DataUtils;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class DiaryActivity3 extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
 
+import lombok.NonNull;
+
+public class DiaryActivity3 extends AppCompatActivity {
     String day;
     Button next_btn;
     ActivityDiary3Binding binding;
@@ -44,28 +53,26 @@ public class DiaryActivity3 extends AppCompatActivity {
 //        day_text.setText(day);
 
 
+
         // 기록버튼
         next_btn = findViewById(R.id.next_btn);
-        next_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateWriting();
-                Intent intent = new Intent(DiaryActivity3.this, DiaryActivity4.class);
-                startActivity(intent);
-                finish();
-            }
+        next_btn.setOnClickListener(v -> {
+            Intent intent1 = new Intent(DiaryActivity3.this, DiaryActivity4.class);
+            startActivity(intent1);
+            finish();
         });
+
+        binding.map.setOnClickListener(new Map());
     }
 
 
-    // 일기 업로드
-    public void updateWriting() {
-        FirebaseFirestore database;
 
-        PostData postData = new PostData();
-        postData.setContent(binding.writing.getText().toString());
-        postData.setUserId(pref.getString("userToken", null));
-
+    private class Map implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Intent mapIntent = new Intent(DiaryActivity3.this, MapActivity.class);
+            startActivityForResult(mapIntent, 0);
+        }
     }
 
 
---
+++ app/src/main/java/com/example/mpteam/MapActivity.java
@@ -0,0 +1,171 @@
+package com.example.mpteam;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+
+import com.example.mpteam.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MapActivity extends AppCompatActivity {
+
+    String [] permission_list = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
+
+    LocationManager locationManager;
+    GoogleMap map;
+    MapView mapView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            requestPermissions(permission_list, 0);
+        }else{
+            new MapThread().start();
+        }
+    }
+
+    public class MapThread extends Thread{
+        @Override
+        public void run() {
+            super.run();
+
+            init();
+
+
+        }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull  int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for(int result : grantResults){
+            if(result == PackageManager.PERMISSION_DENIED){
+                return;
+            }
+        }
+        init();
+    }
+
+    public void init(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        SupportMapFragment mapFragment = (SupportMapFragment)fragmentManager.findFragmentById(R.id.map);
+
+
+        MapReadyCallback callback1 = new MapReadyCallback();
+        mapFragment.getMapAsync(callback1);
+    }
+
+    // 구글 지도 사용 준비가 완료되면 동작하는 콜백
+    class MapReadyCallback implements OnMapReadyCallback {
+        @Override
+        public void onMapReady(@NonNull GoogleMap googleMap) {
+            map = googleMap;
+            //            Log.d("test123", "onMapReady: 구글지도 사용 준비 완료 ");
+            getMyLocation();
+        }
+    }
+
+    // 현재 위치를 측정하는 메서드
+    public void getMyLocation(){
+        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+        // 권한 확인 작업
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED)
+                return;
+        }
+
+        // 이전에 측정했던 값을 가져온다.
+        Location location1 = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Location location2 = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        if(location1 != null) {
+            setMyLocation(location1);
+        }else if(location2 != null)
+            setMyLocation(location2);
+        // 새롭게 측정한다.
+        GetMyLocationListener listener = new GetMyLocationListener();
+
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000, 10f, listener);
+        }
+        if(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 10f, listener);
+        }
+    }
+
+    public void setMyLocation(Location location){
+        String TAG = "test123";
+        Log.d(TAG, "위도: " + location.getLatitude());
+        Log.d(TAG, "경도도: " + location.getLongitude());
+
+        // 위도와 경도값을 관리하는 객체
+        LatLng position = new LatLng(location.getLatitude(), location.getLongitude());
+
+        // 위성 카메라의 위치를 옮긴다.
+        CameraUpdate update1 = CameraUpdateFactory.newLatLng(position);
+        CameraUpdate update2 = CameraUpdateFactory.zoomTo(15f);     // 현재보다 15배 확대
+
+        map.moveCamera(update1);
+        map.animateCamera(update2);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED)
+                return;
+        }
+
+        // 현재 위치 표시
+        map.setMyLocationEnabled(true);
+
+    }
+
+    // 현재 위치 측정이 성공하면 반응하는 리스너
+    class GetMyLocationListener implements LocationListener{
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(@NonNull String provider) {
+
+        }
+        // 위치 측정에 필요한 요소가 사용이 불가능 하게 되었을 때
+        @Override
+        public void onProviderDisabled(@NonNull String provider) {
+
+        }
+
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            setMyLocation(location);
+            //위치 측정 중단.
+        }
+    }
+
+}
