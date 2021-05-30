8df2a5fe5adddfeff96d41701cc37fb25fff1ec0
politics-in-the-pond <74289147+politics-in-the-pond@users.noreply.github.com>
2021-05-30 22:15:52 +0900

    날짜모듈추가

---
+++ app/src/main/java/com/example/mpteam/modules/DateModule.java
@@ -0,0 +1,26 @@
+package com.example.mpteam.modules;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateModule {
+
+    static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+
+    public static int compareDay(String dayOrigin, String dayNew) { //origin : 10일 new : 12일 일때, 2 반환
+        Date origin = null;
+        try {
+            origin = dateFormat.parse(dayOrigin);
+            Date compare = dateFormat.parse(dayNew);
+            return compare.compareTo(origin);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static String getToday(){
+        return dateFormat.format(System.currentTimeMillis());
+    }
+}
